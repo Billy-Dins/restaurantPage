8176d9a51383bb78bfebbf7c87adcef08361a494
Billy <Lrdweaver@gmail.com>
2023-01-13 20:13:07 -0500

    theme change to Latvian

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import skates from '../assets/imgs/skates.jpg';
+import mikelbaka from '../assets/imgs/Mikelbaka.jpg'
 
 const home = function() {
     const content = document.querySelector('#content');
@@ -21,10 +22,39 @@ const home = function() {
     aboutHeader.textContent = 'About'
     headerContent.appendChild(aboutHeader);
 
+    const bookHeader = document.createElement('button');
+    bookHeader.classList.add('headerItem');
+    bookHeader.textContent = 'Book';
+    headerContent.appendChild(bookHeader);
+
     const background = document.createElement('img');
-    background.src = skates;
+    background.src = mikelbaka;
     background.classList.add('background');
     content.appendChild(background);
+
+    const footer = document.createElement('footer');
+    footer.classList.add('footer');
+    content.appendChild(footer);
+
+    const footerItemOne = document.createElement('div');
+    footerItemOne.classList.add('footerItem');
+    footerItemOne.textContent = 'Authentic Latvian Cuisine'
+    footer.appendChild(footerItemOne);
+
+    const footerItemTwo = document.createElement('div');
+    footerItemTwo.classList.add('footerItem');
+    footerItemTwo.textContent = '$'
+    footer.appendChild(footerItemTwo);
+
+    const footerItemThree = document.createElement('div');
+    footerItemThree.classList.add('footerItem');
+    footerItemThree.textContent = 'Casual'
+    footer.appendChild(footerItemThree);
+
+    const footerItemFour = document.createElement('div');
+    footerItemFour.classList.add('footerItem');
+    footerItemFour.textContent = '420-696-6969'
+    footer.appendChild(footerItemFour);
 };
 
 export default home
